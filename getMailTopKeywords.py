f4a27c466c57e7179b0c6fd1cafddd4ed1532572
Edgar Salgado <edsalsen@gmail.com>
2013-10-20 16:59:56 +0200

    -Added the option to strip words to its root word using NLTK library

--- getMailTopKeywords.py
+++ getMailTopKeywords.py
@@ -1,29 +1,52 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
-import os
-import time
-import math
-from HTMLParser import HTMLParser
 import argparse
-
-try:
-    import pyzmail
-except ImportError:
-    if os.path.isdir('../pyzmail'):
-        sys.path.append(os.path.abspath('..'))
-    elif os.path.isdir('pyzmail'):
-        sys.path.append(os.path.abspath('.'))
-    import pyzmail
+from HTMLParser import HTMLParser
+import math
+import os
+import pyzmail #http://www.magiksys.net/pyzmail/#
+import sys
+import time
+import unicodedata
 
 def main(argv):
     args = optionParser(argv)
-    if not args.count:
-        print('Reading from %s ... ' % args.folder)
-        extractMailFromPath(args.folder, args.count)
+    print('Reading from %s ... ' % args.folder)
+    text = extractMailFromPath(args.folder)
+    
+    omitwords = decode('from to re de para subject asunto date fecha escribió de que no a la el es y en lo un por me qué una te los se con para mi está si pero las su yo tu del al como le eso sí esta ya más muy hay bien estoy todo nos tengo ha este cuando sólo vamos cómo estás o soy puedo esto quiero aquí tiene tú ahora algo fue son ser he era eres así sé tiene ese bueno creo todos sus puede voy tan esa porque dónde hacer quién nunca nada él estaba están quieres va sabes vez hace ella dos tenemos puedes sin hasta sr és per dels jo amb com ho has').split()
+     #https://es.wiktionary.org/wiki/Ap%C3%A9ndice:Palabras_m%C3%A1s_frecuentes_del_espa%C3%B1ol
+
+    language = 'es'    
+    if args.lang:
+        language = args.lang
+    
+    count = 10        
+    if args.count:
+        count = args.count
+    
+    print "Building a list of the top keywords..."
+    if args.aspell:
+        wordsInDict, wordsNotInDict = getWordsFromDictionary(language, text, omitwords)
     else:
-        extractMailFromPath(args.folder, 10)
+        wordsInDict = getWords(text, omitwords)
+
+    words = wordsInDict
+    if args.nltk:
+        print "Processing using Natural Language Toolkit..."
+        words = stemWords(words)
+    
+    returnRepetitions(words, count)
+    
+def stemWords(list):
+    #http://nltk.org/index.html
+    from nltk.stem import SnowballStemmer
+    st = SnowballStemmer("spanish")
+    stemedList = []
+    for word in list:
+        stemedList.append(st.stem(word))
+    return stemedList
 
 def optionParser(argv):
     parser = argparse.ArgumentParser()
@@ -31,6 +54,14 @@ def optionParser(argv):
                         type=str)
     parser.add_argument("-c", "--count", help="show words with count greater or equal than this number. Default: 10",
                         type=int)
+    parser.add_argument("-a", "--aspell", help="whether to check the words using Aspell. Requires python-aspell library. See https://github.com/WojciechMula/aspell-python. Default: no",
+                        action="store_true")
+
+    parser.add_argument("-l", "--lang", help="check words in that language (only if -d is set). Default: es",
+                        type=str)
+
+    parser.add_argument("-n", "--nltk", help="use Natural Language Toolkit. Requires nltk library. See http://nltk.org/index.html. Default: no",
+                        action="store_true")                        
                         
     args = parser.parse_args()
     
@@ -39,50 +70,32 @@ def optionParser(argv):
     else:
         sys.exit('Sorry: directory %s does not exist!' % args.folder)
 
-def buildDictionary():
-    return True
-
-def extractMailFromPath(path, repetitions):
-    numberOfFiles = 0
-    for root, subFolders, files in os.walk(path):
-        for filename in files[1:]:
-            numberOfFiles += 1
-    print "Analyzing %s mails..." % numberOfFiles
-    counter = 0
-    text = ""
-    for root, subFolders, files in os.walk(path):
-        for filename in files[1:]:
-            counter += 1
-            progress(50, counter*100/numberOfFiles)
-            filePath = os.path.join(root, filename)
-            text = text + extractMailFromFile(filePath)
-
-    getTopKeywords(text, repetitions)
-
-def extractMailFromFile(filename):
-    #print filename
-    msg=pyzmail.PyzMessage.factory(open(filename, 'rb'))
-    #print len(msg.mailparts)
-    #print msg.mailparts
-    text = ""
-    for mailpart in msg.mailparts:
-        if mailpart.is_body=='text/plain':
-        #if mailpart.is_body=='text/html':
-            #print mailpart.charset
-            payload, used_charset=pyzmail.decode_text(mailpart.get_payload(), mailpart.charset, None) 
-            for line in payload.replace('\r',' ').split('\n'):
-                # omit lines starting with >, which is normally a quote for previous mail
-                if not line.startswith('>'):
-                    text = text + line
-    return text
-
-def getTopKeywords(text, repetitions):
-    print "Building a list of the top keywords..."
+def getWordsFromDictionary(language, text, omitwords):
+    import aspell
+    #http://mx.answers.yahoo.com/question/index?qid=20090923123859AA0EO5m
+    s = aspell.Speller('lang', language)
+    text = unicodedata.normalize('NFKD', text).encode('ascii','ignore')
+    wordList1 = []
+    wordList1 = text.lower().split(None)
+    wordListInDictionary = []
+    wordListNotInDictionary = []
+    for word1 in wordList1:
+        lastchar = word1[-1:]
+        if lastchar in [",", ".", "!", "?", ";", ":"]:
+            word2 = word1.rstrip(lastchar)
+        else:
+            word2 = word1
+        if len(word2) > 1 and s.check(word2) and word2 not in omitwords:
+            wordListInDictionary.append(word2)
+        else:
+            if word2 not in omitwords:
+                wordListNotInDictionary.append(word2)
+    
+    return wordListInDictionary, wordListNotInDictionary
+    
+def getWords(text, omitwords):
     #http://programming-review.com/top-keywords-in-python/
-    #https://es.wiktionary.org/wiki/Ap%C3%A9ndice:Palabras_m%C3%A1s_frecuentes_del_espa%C3%B1ol
-    omitwords = decode('from: to: re: cc: de: para: subject: asunto: date: fecha: escribió: de que no a la el es y en lo un por me qué una te los se con para mi está si pero las su yo tu del al como le eso sí esta ya más muy hay bien estoy todo nos tengo ha este cuando sólo vamos cómo estás o soy puedo esto quiero aquí tiene tú ahora algo fue son ser he era eres así sé tiene ese bueno creo todos sus puede voy tan esa porque dónde hacer quién nunca nada él estaba están quieres va sabes vez hace ella dos tenemos puedes sin hasta sr és per dels jo amb com ho has').split()
     omitcharacterlist = '@ ( / -'.split()
-    print "Top keywords:"
     wordList1 = []
     wordList1 = text.lower().split(None)
     wordList2 = []
@@ -96,11 +109,15 @@ def getTopKeywords(text, repetitions):
         if len(word2.lower()) > 1 and word2.lower() not in omitwords:
             if not any(character in word2.lower() for character in omitcharacterlist):
                 wordList2.append(word2.lower())
-        
+    
+    return wordList2
+    
+def returnRepetitions(wordlist, repetitions):
+    print "Top keywords:"
     # create word frequency dictionary  = hashtable
     Dict = {}
-    for word2 in wordList2:
-        Dict[word2] = Dict.get(word2, 0) + 1
+    for word in wordlist:
+        Dict[word] = Dict.get(word, 0) + 1
     
     # create a list of keys and sort the list
     keys = Dict.keys()
@@ -114,7 +131,47 @@ def getTopKeywords(text, repetitions):
     for item in items:
         if item[1] > repetitions:
             print " ", item[0], item[1]
-    
+
+def extractMailFromPath(path):
+    numberOfFiles = 0
+    for root, subFolders, files in os.walk(path):
+        for filename in files[1:]:
+            numberOfFiles += 1
+    print "Analyzing %s mails..." % numberOfFiles
+    counter = 0
+    text = ""
+    for root, subFolders, files in os.walk(path):
+        for filename in files[1:]:
+            counter += 1
+            progress(50, counter*100/numberOfFiles)
+            filePath = os.path.join(root, filename)
+            text = text + extractMailFromFile(filePath)
+    return text
+
+def extractMailFromFile(filename):
+    msg=pyzmail.PyzMessage.factory(open(filename, 'rb'))
+    text = ""
+    '''if len(msg.mailparts) > 1:
+        for mailpart in msg.mailparts:
+            if mailpart.is_body=='text/html':
+                #print mailpart.charset
+                payload, used_charset=pyzmail.decode_text(mailpart.get_payload(), mailpart.charset, None) 
+                for line in payload.replace('\r',' ').split('\n'):
+                    # omit lines starting with >, which is normally a quote for previous mail
+                    if not line.startswith('>'):
+                        text = text + line
+        text = strip_tags(text)
+    else:'''
+    for mailpart in msg.mailparts:
+        if mailpart.is_body=='text/plain':
+            payload, used_charset=pyzmail.decode_text(mailpart.get_payload(), mailpart.charset, None) 
+            for line in payload.replace('\r',' ').split('\n'):
+                # omit lines starting with >, which is normally a quote for previous mail
+                if not line.startswith('>'):
+                    text = text + line
+
+    return text
+
 # http://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -153,4 +210,7 @@ def progress(width, percent):
     sys.stdout.flush()
     
 if __name__ == "__main__":
-    main(sys.argv)
+    try:
+        main(sys.argv)
+    except KeyboardInterrupt:
+        print "\nCtrl+C detected, exiting."
